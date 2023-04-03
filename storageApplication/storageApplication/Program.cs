79fc7c83164575646722543e09c323cda4b5897c
zbbhangzhi <30279203+zbbhangzhi@users.noreply.github.com>
2023-04-03 22:22:02 +0100

    task 5: add repository to scope

--- storageApplication/storageApplication/Program.cs
+++ storageApplication/storageApplication/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+using Models;
+using Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IRepository, BaseRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
